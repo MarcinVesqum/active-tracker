cb204952fdc4f267efd1c6a339081b8d1e26b306
Vesqum <marcin.pelszyk30@gmail.com>
2022-08-24 15:17:09 +0200

    change supabase envvCmi

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.vue"] ,
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
