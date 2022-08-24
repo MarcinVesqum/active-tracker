baecb73d1098b2d020be0c5b041a36ea0136e91d
Vesqum <marcin.pelszyk30@gmail.com>
2022-08-24 14:32:24 +0200

    change supabase env

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import {supabase} from '../supbase/supabase' 
+import {supabase} from '../supbase' 
 
 
 const routes = [
