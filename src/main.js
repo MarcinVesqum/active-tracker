2468164ceb983ead63f152ae79721951965833ed
Vesqum <marcin.pelszyk30@gmail.com>
2022-08-24 16:04:28 +0200

    last changed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './index.css'
-import './main.css'
 import router from './router/router.js';
 import store from './store/index.js'
 import App from './App.vue'
