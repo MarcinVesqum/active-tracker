46d7cdc6f67855ec6346571d58d7a44619e77760
Vesqum <marcin.pelszyk30@gmail.com>
2022-08-24 16:11:33 +0200

    last changed

--- src/supbase/supabase.js
+++ src/supbase/supabase.js
@@ -3,9 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.VITE_SUPABASE_URL
 const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-
-export default function useSupabase() {
-    return { supabase };
-  }
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader';
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), svgLoader()],
-  define: {
-    'process.env': process.env
-  }
-})
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+  return defineConfig({
+      plugins: [vue(), svgLoader()],
+  });
+
+}
