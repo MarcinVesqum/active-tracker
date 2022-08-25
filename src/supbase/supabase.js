9bcbcd685a5b5fb51855ec3952fd60a6893e458e
Vesqum <marcin.pelszyk30@gmail.com>
2022-08-25 14:19:54 +0200

    confing: added delpoly file

--- src/supbase/supabase.js
+++ src/supbase/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseUrl = 'https://nuuitcahtqtagltvydlq.supabase.co'
 const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
